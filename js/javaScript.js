19dd503cee0213f015aa35dad38816dfcd5351a6
abdelhakim tamoum <tamoum.hakim@gmail.com>
2021-04-18 23:06:39 -0400

    modifier mot pass JS

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,8 +1,10 @@
 /* global console, alert, prompt */
 
+/*
 var motPass = prompt('ecrire le mot pass :');
 
 if (motPass !== "hakim18") {
     document.getElementById('hakim').innerHTML = 
         "le mot pass " + motPass + " n'est pas correct";
 }
+*/
