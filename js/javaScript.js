503fce77104db4cecba6a22e0e89f9a660d3e1aa
abdelhakim tamoum <tamoum.hakim@gmail.com>
2021-05-31 11:29:04 -0400

    modifier image d'ecran

--- js/javaScript.js
+++ js/javaScript.js
@@ -8,3 +8,16 @@ if (motPass !== "hakim18") {
         "le mot pass " + motPass + " n'est pas correct";
 }
 */
+// select landing page element
+let landingPage = document.querySelector(".slider");
+
+// get array of imgs
+let imgsArray = ["ecran1.jpg","ecran2.jpg","ecran3.jpg","ecran4.jpg"];
+
+setInterval(function() { //setInterval(() => { 
+    //get random number الرقم العشوائي
+    let randomNumber = Math.floor(Math.random() * imgsArray.length);
+
+    //change background image url
+    landingPage.style.backgroundImage = 'url("images/' + imgsArray[randomNumber] + '")';
+}, 3000);
